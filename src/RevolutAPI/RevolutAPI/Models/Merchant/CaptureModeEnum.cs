09704f3c90cdb4cef2bffef04d47995793baf5a8
Gregor Spagnolo <gregor@ssrd.io>
2020-06-03 20:57:14 +0200

    added merchant API support

---
+++ src/RevolutAPI/RevolutAPI/Models/Merchant/CaptureModeEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RevolutAPI.Models.Merchant
+{
+    public enum CaptureModeEnum
+    {
+        AUTOMATIC, MANUAL
+    }
+}
---
+++ src/RevolutAPI/RevolutAPI/Models/Merchant/RefundOrdredReq.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RevolutAPI.Models.Merchant
+{
+    public class RefundOrdredReq
+    {
+
+        [JsonProperty("amount")]
+        public long Amount { get; set; }
+        
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("merchant_customer_ext_ref")]
+        public string MerchantCustomerExtRef { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
