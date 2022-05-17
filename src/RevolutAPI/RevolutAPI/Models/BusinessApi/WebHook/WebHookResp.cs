bb10b74eebae824968c80cea88968b4902522116
Amadej Krepek <amadej.krepek@ssrd.io>
2022-05-17 16:22:27 +0200

    Implemented endpoints hook

---
+++ src/RevolutAPI/RevolutAPI/Models/BusinessApi/WebHook/WebHookResp.cs
@@ -0,0 +1,9 @@
+﻿namespace RevolutAPI.Models.BusinessApi.WebHook
+{
+    public class WebHookResp
+    {
+        public int Code { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ src/RevolutAPI/RevolutAPI/OutCalls/BusinessApi/WebHookApiClient.cs
@@ -0,0 +1,35 @@
+﻿using RevolutAPI.Helpers;
+using RevolutAPI.Models.BusinessApi.WebHook;
+using System.Threading.Tasks;
+
+namespace RevolutAPI.OutCalls.BusinessApi
+{
+    public class WebHookApiClient
+    {
+        private readonly IRevolutApiClient _apiClient;
+
+        public WebHookApiClient(IRevolutApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task<WebHookResp> GetWebHook()
+        {
+            string endpoint = "/webhook";
+            return await _apiClient.Get<WebHookResp>(endpoint);
+        }
+
+        public async Task<Result<WebHookResp>> CreateWebHook(AddWebHookReq req)
+        {
+            string endpoint = "/webhook";
+            Result<WebHookResp> result = await _apiClient.Post<WebHookResp>(endpoint, req);
+            return result;
+        }
+
+        public async Task<bool> DeleteWebHook()
+        {
+            string endpoint = "/webhook";
+            return await _apiClient.Delete(endpoint);
+        }
+    }
+}
